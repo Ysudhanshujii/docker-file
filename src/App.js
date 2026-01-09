e769b93d74cb1a248349697b8c6edcd88869ce39
Ysudhanshujii <sudhanshuyadav202545@gmail.com>
2026-01-09 17:05:31 +0530

    Dockerized Node.js web application

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         this is demo project for docker tutorial build testing.
         </p>
         <a
           className="App-link"
